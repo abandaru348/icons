289e496b107ecf8aa0240681ec77fe1eb8c805b7
abandaru348 <anushab.sfdc@gmail.com>
2025-11-10 12:09:39 -0600

    Update gnalTileComponent.js

--- force-app/main/default/lwc/gnalTileComponent/gnalTileComponent.js
+++ force-app/main/default/lwc/gnalTileComponent/gnalTileComponent.js
@@ -1,39 +1,32 @@
 import { LightningElement, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 
+
 export default class GnalTileComponent extends NavigationMixin(LightningElement) {
-  static DEFAULT_FOOTER_LABEL = 'Learn More';
 
-  @api title;
-  @api description;
-  @api links = [];
-  @api learnMoreUrl;
-  @api footerUrl;
-  @api footerLabel;
+    static DEFAULT_FOOTER_LABEL = 'Learn More';
 
-  get footerUrlResolved() {
+    @api title;
+    @api description;
+    @api links = [];
+    @api learnMoreUrl;
+    @api footerUrl;         
+    @api footerLabel; 
+
+    get footerUrlResolved() {
     return this.footerUrl || this.learnMoreUrl;
   }
 
   get footerLabelResolved() {
+
     return this.footerLabel || this.constructor.DEFAULT_FOOTER_LABEL;
   }
+   
 
-  handleLearnMore(event) {
+   handleLearnMore(event) {
     const url = event.currentTarget.dataset.url || this.footerUrlResolved;
     this.navigateToUrl(url);
   }
 
-  navigateToUrl(url) {
-    if (!url) {
-      return;
-    }
-
-    this[NavigationMixin.Navigate]({
-      type: 'standard__webPage',
-      attributes: {
-        url,
-      },
-    });
-  }
+  
 }
