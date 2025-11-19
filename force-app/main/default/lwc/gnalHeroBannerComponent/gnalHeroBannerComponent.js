d9f5247525ffd45fc7638ce6928accab7dee44bc
abandaru348 <anushab.sfdc@gmail.com>
2025-11-19 10:37:16 -0600

    Update gnalHeroBannerComponent.js

--- force-app/main/default/lwc/gnalHeroBannerComponent/gnalHeroBannerComponent.js
+++ force-app/main/default/lwc/gnalHeroBannerComponent/gnalHeroBannerComponent.js
@@ -1,15 +1,16 @@
+
 import { LightningElement, api } from 'lwc';
 import heroImage from '@salesforce/resourceUrl/gnalfam';
 
 export default class GnalHeroBannerComponent extends LightningElement {
     @api height = '260px';
     @api radius = '12px';
-    @api imagePath = heroImage;
+    imagePath = heroImage;
     @api heroAriaLabel = 'Family smiling outside; My Military Health hero banner';
 
     get bannerStyle() {
         return `height:${this.height};border-radius:${this.radius};background-image:url(${this.imagePath});`;
     }
 
-    // No runtime handlers needed; image is rendered via CSS background
+    
 }
